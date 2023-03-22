f3970ddb51253f69c43f70dc3b4ef148c5401e33
Youn_Ji <syj9760@gmail.com>
2023-03-22 20:34:43 +0900

    feat: ResourceItem 컴포넌트 구현

---
+++ src/features/common/components/Input.tsx
@@ -0,0 +1,27 @@
+import { styled } from "../../../stitches.config"
+
+export const Input = styled("input", {
+  all: "unset",
+  boxSizing: "border-box",
+  width: "100%",
+  display: "inline-flex",
+  alignItems: "center",
+  justifyContent: "center",
+  borderRadius: "3px",
+  fontSize: "12px",
+  fontWeight: 400,
+  color: "$black",
+  padding: "0 8px",
+  height: "30px",
+  lineHeight: 1,
+  boxShadow: "0 0 0 1px $colors$blue50",
+  background: "$gray97",
+
+  variants: {
+    isError: {
+      true: {
+        boxShadow: "0 0 0 1px $colors$red",
+      },
+    },
+  },
+})
